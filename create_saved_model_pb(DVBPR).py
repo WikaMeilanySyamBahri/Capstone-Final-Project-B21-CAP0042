ce1cc6197391a3f61fe96bb67e2a663495835ddf
Wika Meilany <74451315+WikaMeilanySyamBahri@users.noreply.github.com>
2021-06-10 00:31:04 +0700

    DVBPR model with format .pb

---
+++ create_saved_model_pb(DVBPR).py
@@ -0,0 +1,22 @@
+import os
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.tools import freeze_graph
+
+# Freeze the graph
+save_path="/home/checkpoint/" #directory to model files
+MODEL_NAME = 'Freeze_model' #name of the model optional
+input_graph_path = save_path+'savegraph.pbtxt'#complete path to the input graph
+checkpoint_path = save_path+'DVBPR_auc_100_39728.ckpt' #complete path to the model's checkpoint file
+input_saver_def_path = ""
+input_binary = False
+output_node_names = "output" #output node's name. Should match to that mentioned in your code
+restore_op_name = "save/restore_all"
+filename_tensor_name = "save/Const:0"
+output_frozen_graph_name = save_path+'frozen_model_'+MODEL_NAME+'.pb' # the name of .pb file you would like to give
+clear_devices = True
+
+freeze_graph.freeze_graph(input_graph_path, input_saver_def_path,
+                          input_binary, checkpoint_path, output_node_names,
+                          restore_op_name, filename_tensor_name,
+                          output_frozen_graph_name, clear_devices, "")
